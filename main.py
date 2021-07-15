731fe9b10f75fd79a523b45e2aaff7550cd95bf3
Aliaksandr <Nicepk.by@gmail.com>
2021-07-15 18:11:00 +0200

    first commit4

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
-import telebot
-import os
-from flask import Flask, request
 import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
+PORT = int(os.environ.get('PORT', 5000))
 
-bot = telebot.TeleBot('1835876628:AAGmblR_mTmz-p-7QpB7g-QRehHaYFpN3Ig')
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-logger = telebot.logger
-telebot.logger.setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
+TOKEN = '1835876628:AAGmblR_mTmz-p-7QpB7g-QRehHaYFpN3Ig'
 
-server = Flask(__name__)
-@server.route("/bot", methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://andreewtelegabot.herokuapp.com/")
-    return "?", 200
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def main():
+    """Start the bot."""
+    updater = Updater(TOKEN, use_context=True)
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    dp.add_error_handler(error)
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://andreewtelegabot.herokuapp.com/' + TOKEN)
+
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
