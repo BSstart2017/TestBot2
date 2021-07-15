9d12c48a56ea6beeef3012967d395d8fac7fdd68
Aliaksandr <Nicepk.by@gmail.com>
2021-07-15 12:47:37 +0200

    first commit3

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-from flask import Flask, request
 import telebot
-
 bot = telebot.TeleBot('1835876628:AAGmblR_mTmz-p-7QpB7g-QRehHaYFpN3Ig')
-bot.set_webhook(url="https://andreewtelegabot.herokuapp.com/")
-app = Flask(__name__)
+  
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "/start":
+        bot.send_message(message.from_user.id, "Введите пароль")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /start.")
 
 
-@app.route('/', methods=["POST"])
-def webhook():
-    bot.process_new_updates(
-        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))]
-    )
-    return "ok"
 
 
-@bot.message_handler(commands=['start'])
-def start_command(message):
-    bot.send_message(message.chat.id, 'Hello!')
-
-
-if __name__ == "__main__":
-    app.run()
+bot.polling(none_stop=True)
