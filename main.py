fcb40d713394471efb528aa287500bb950a85b28
Aliaksandr <Nicepk.by@gmail.com>
2021-07-15 17:44:31 +0200

    first commit4

--- main.py
+++ main.py
@@ -5,26 +5,17 @@ import logging
 
 bot = telebot.TeleBot('1835876628:AAGmblR_mTmz-p-7QpB7g-QRehHaYFpN3Ig')
 
-# Здесь пишем наши хэндлеры
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
 
-# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
-if "HEROKU" in list(os.environ.keys()):
-    logger = telebot.logger
-    telebot.logger.setLevel(logging.INFO)
-
-    server = Flask(__name__)
-    @server.route("/bot", methods=['POST'])
-    def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
-    @server.route("/")
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url="https://andreewtelegabot.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
-        return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
-else:
-    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.  
-    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+server = Flask(__name__)
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+@server.route("/")
+def webhook():
     bot.remove_webhook()
-    bot.polling(none_stop=True)
+    bot.set_webhook(url="https://andreewtelegabot.herokuapp.com/bot")
+    return "?", 200
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
