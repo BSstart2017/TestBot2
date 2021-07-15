06f972ec5bc284eaa34b89589d2d648cae1a2aeb
Aliaksandr <Nicepk.by@gmail.com>
2021-07-15 12:56:04 +0200

    first commit4

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import telebot
+import os
+from flask import Flask, request
+import logging
+
 bot = telebot.TeleBot('1835876628:AAGmblR_mTmz-p-7QpB7g-QRehHaYFpN3Ig')
-  
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "/start":
-        bot.send_message(message.from_user.id, "Введите пароль")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /start.")
 
+# Здесь пишем наши хэндлеры
 
+# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
+if "HEROKU" in list(os.environ.keys()):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
 
-
-bot.polling(none_stop=True)
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://andreewtelegabot.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
+        return "?", 200
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
+    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.  
+    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
