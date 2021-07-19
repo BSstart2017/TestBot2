e7530bf0dba206f528a847e72773f542ec4a5e6e
Aliaksandr <Nicepk.by@gmail.com>
2021-07-19 07:59:10 +0200

    first commit12

--- bot/bot.py
+++ bot/bot.py
@@ -9,8 +9,17 @@ from bot.settings import (BOT_TOKEN, HEROKU_APP_NAME,
                           WEBAPP_HOST, WEBAPP_PORT)
 import psycopg2
 import psycopg2.extras
-import keyboards as kb
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
+inline_btn_1 = InlineKeyboardButton('Текущий баланс', callback_data='button1')
+inline_btn_2 = InlineKeyboardButton('Создать карточку', callback_data='button2')
+inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1, inline_btn_2)
+
+inline_btn_3 = InlineKeyboardButton('Сегодня', callback_data='button3')
+inline_btn_4 = InlineKeyboardButton('Календарь', callback_data='button4')
+inline_btn_5 = InlineKeyboardButton('Отмена', callback_data='button5')
+inline_kb2 = InlineKeyboardMarkup().add(inline_btn_3, inline_btn_4, inline_btn_5)
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
