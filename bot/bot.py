0cc58708c134f37db87b6b2a893b24565131ea61
Aliaksandr <Nicepk.by@gmail.com>
2021-07-19 06:15:50 +0200

    first commit12

--- bot/bot.py
+++ bot/bot.py
@@ -7,10 +7,14 @@ from aiogram.utils.executor import start_webhook
 from bot.settings import (BOT_TOKEN, HEROKU_APP_NAME,
                           WEBHOOK_URL, WEBHOOK_PATH,
                           WEBAPP_HOST, WEBAPP_PORT)
+import psycopg2
+import psycopg2.extras
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
+
+conn = psycopg2.connect(dbname='dd93h7g3uedrn1', user='soxrigiqvchsmn', password='535a584a9a46fa70752593b7f9ec8a7927c6f377515fbb2f87f0fc52c1bb3fb7', host='ec2-23-20-124-77.compute-1.amazonaws.com')
 
 botlog = []
 
@@ -46,3 +50,4 @@ def main():
         port=WEBAPP_PORT,
     )
 
+conn.close()
