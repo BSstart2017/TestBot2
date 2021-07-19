aa04fe3128f591070e63c73513a22d47093e78df
Aliaksandr <Nicepk.by@gmail.com>
2021-07-19 07:36:55 +0200

    first commit12

--- bot/bot.py
+++ bot/bot.py
@@ -8,6 +8,7 @@ from bot.settings import (BOT_TOKEN, HEROKU_APP_NAME,
                           WEBHOOK_URL, WEBHOOK_PATH,
                           WEBAPP_HOST, WEBAPP_PORT)
 import psycopg2
+import psycopg2.extras
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
